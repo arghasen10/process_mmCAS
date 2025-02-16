83554ae0a51201a81a9285495fcf7d8d2b1a1a84
arghasen10 <arghasen10@gmail.com>
2025-02-17 04:17:34 +0530

    added range azimuth plotter

--- dummyRunner.m
+++ dummyRunner.m
@@ -1,10 +1,19 @@
 function main()
     frameIdx = 36;
     frameCountGlobal = frameIdx;
-    [radar_data_Rxchain_master] = readBinFile("C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\cascade_cpu_head_1\master_0000_data.bin", frameIdx,256,12,64,4,1);
-    [radar_data_Rxchain_slave1] = readBinFile("C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\cascade_cpu_head_1\slave1_0000_data.bin", frameIdx,256,12,64,4,1);
-    [radar_data_Rxchain_slave2] = readBinFile("C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\cascade_cpu_head_1\slave2_0000_data.bin", frameIdx,256,12,64,4,1);
-    [radar_data_Rxchain_slave3] = readBinFile("C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\cascade_cpu_head_1\slave3_0000_data.bin", frameIdx,256,12,64,4,1);
+    if ismac
+        filepath = '/Users/arghasen/Downloads/cascade_cpu_head_1/';
+    elseif ispc
+        filepath = 'C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\PostProc\cascade_cpu_head_1\';
+    end
+    masterpath = fullfile(filepath, 'master_0000_data.bin');
+    slave1path = fullfile(filepath, 'slave1_0000_data.bin');
+    slave2path = fullfile(filepath, 'slave2_0000_data.bin');
+    slave3path = fullfile(filepath, 'slave3_0000_data.bin');
+    [radar_data_Rxchain_master] = readBinFile(masterpath, frameIdx,256,12,64,4,1);
+    [radar_data_Rxchain_slave1] = readBinFile(slave1path, frameIdx,256,12,64,4,1);
+    [radar_data_Rxchain_slave2] = readBinFile(slave2path, frameIdx,256,12,64,4,1);
+    [radar_data_Rxchain_slave3] = readBinFile(slave3path, frameIdx,256,12,64,4,1);
     radar_data_Rxchain(:,:,1:4,:) = radar_data_Rxchain_master;
     radar_data_Rxchain(:,:,5:8,:) = radar_data_Rxchain_slave1;
     radar_data_Rxchain(:,:,9:12,:) = radar_data_Rxchain_slave2;
@@ -797,5 +806,103 @@ function [peakVal, peakLoc] = DOA_BF_PeakDet_loc(inData)
     for ind = 1:numMax
         peakVal(ind) = maxData(ind,2);
         peakLoc(ind) = rem(maxData(ind,1)-1, N)+1;
+    end
+end
+
+
+function  [mag_data_static mag_data_dynamic y_axis x_axis] = plot_range_azimuth_2D(range_resolution, radar_data_pre_3dfft,TDM_MIMO_numTX,numRxAnt,antenna_azimuthonly, LOG, STATIC_ONLY, PLOT_ON, minRangeBinKeep,  rightRangeBinDiscard)
+    dopplerFFTSize = size(radar_data_pre_3dfft,2);
+    rangeFFTSize = size(radar_data_pre_3dfft,1);
+    angleFFTSize = 256;
+    % ratio used to decide engergy threshold used to pick non-zero Doppler bins
+    ratio = 0.5;
+    DopplerCorrection = 0;
+    
+    if DopplerCorrection == 1
+        % add Doppler correction before generating the heatmap
+        radar_data_pre_3dfft_DopCor= [];
+        for dopplerInd = 1: dopplerFFTSize
+            deltaPhi = 2*pi*(dopplerInd-1-dopplerFFTSize/2)/( TDM_MIMO_numTX*dopplerFFTSize);
+            sig_bin_org =squeeze(radar_data_pre_3dfft(:,dopplerInd,:));
+            for i_TX = 1:TDM_MIMO_numTX
+                RX_ID = (i_TX-1)*numRxAnt+1 : i_TX*numRxAnt;
+                corVec = repmat(exp(-1j*(i_TX-1)*deltaPhi), rangeFFTSize, numRxAnt);
+                radar_data_pre_3dfft_DopCor(:,dopplerInd, RX_ID)= sig_bin_org(:,RX_ID ).* corVec;
+            end
+        end
+        
+        radar_data_pre_3dfft = radar_data_pre_3dfft_DopCor;
+    end
+    radar_data_pre_3dfft = radar_data_pre_3dfft(:,:,antenna_azimuthonly);
+    
+    radar_data_angle_range = fft(radar_data_pre_3dfft, angleFFTSize, 3);
+    n_angle_fft_size = size(radar_data_angle_range,3);
+    n_range_fft_size = size(radar_data_angle_range,1);
+    
+    
+    %decide non-zerp doppler bins to be used for dynamic range-azimuth heatmap
+    DopplerPower = sum(mean((abs(radar_data_pre_3dfft(:,:,:))),3),1);
+    DopplerPower_noDC = DopplerPower([1: dopplerFFTSize/2-1 dopplerFFTSize/2+3:end]);
+    [peakVal peakInd] = max(DopplerPower_noDC);
+    threshold = peakVal*ratio;
+    indSel = find(DopplerPower_noDC >threshold);
+    for ii = 1:length(indSel)
+        if indSel(ii) > dopplerFFTSize/2-1
+            indSel(ii) = indSel(ii) + 3;
+        end
+    end
+    
+    radar_data_angle_range_dynamic = squeeze(sum(abs(radar_data_angle_range(:,indSel,:)),2));
+    radar_data_angle_range_Static = squeeze(sum(abs(radar_data_angle_range(:,dopplerFFTSize/2+1,:)),2));
+    
+    
+    indices_1D = (minRangeBinKeep:n_range_fft_size-rightRangeBinDiscard);
+    max_range = (n_range_fft_size-1)*range_resolution;
+    max_range = max_range/2;
+    d = 1;
+    
+    %generate range/angleFFT for zeroDoppler and non-zero Doppler respectively
+    radar_data_angle_range_dynamic = fftshift(radar_data_angle_range_dynamic,2);
+    radar_data_angle_range_Static = fftshift(radar_data_angle_range_Static,2);
+    
+    
+    sine_theta = -2*((-n_angle_fft_size/2:n_angle_fft_size/2)/n_angle_fft_size)/d;
+    cos_theta = sqrt(1-sine_theta.^2);
+    
+    [R_mat, sine_theta_mat] = meshgrid(indices_1D*range_resolution,sine_theta);
+    [~, cos_theta_mat] = meshgrid(indices_1D,cos_theta);
+    
+    x_axis = R_mat.*cos_theta_mat;
+    y_axis = R_mat.*sine_theta_mat;
+    mag_data_dynamic = squeeze(abs(radar_data_angle_range_dynamic(indices_1D+1,[1:end 1])));
+    mag_data_static = squeeze(abs(radar_data_angle_range_Static(indices_1D+1,[1:end 1])));
+    
+    
+    mag_data_dynamic = mag_data_dynamic';
+    mag_data_static = mag_data_static';
+    mag_data_dynamic = flipud(mag_data_dynamic);
+    mag_data_static = flipud(mag_data_static);
+    
+    
+    if PLOT_ON
+        log_plot = LOG;
+        if STATIC_ONLY == 1
+            if log_plot
+                surf(y_axis, x_axis, (mag_data_static).^0.4,'EdgeColor','none');
+            else
+                surf(y_axis, x_axis, abs(mag_data_static),'EdgeColor','none');
+            end
+        else
+            if log_plot
+                surf(y_axis, x_axis, (mag_data_dynamic).^0.4,'EdgeColor','none');
+            else
+                surf(y_axis, x_axis, abs(mag_data_dynamic),'EdgeColor','none');
+            end
+        end
+        
+        view(2);
+        xlabel('meters')
+        ylabel('meters')
+        
     end
 end
